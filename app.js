0751847bc9949e1f25d0fc56f42ea7c91331c979
NatusyaZira <natusya.zira@gmail.com>
2023-02-20 20:27:04 +0200

    Add Enviroment Vars

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const { log } = require('console');
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -22,7 +26,7 @@ const userSchema = new mongoose.Schema ({
 
 const secret = "Thisisourlittlesecret.";
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
